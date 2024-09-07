58ad75852b0cd8f01b7f2813e91f2b8f6d0474e2
MariaWetterlein <wetterleinmarialudmila@gmail.com>
2024-09-07 09:02:58 -0300

    criei as classes livro e empréstimo

---
+++ SRC/Entites/Livro.cs
@@ -0,0 +1,39 @@
+
+using System;
+namespace SRC.Entities
+{
+    public class Livro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string ISBN { get; set; }
+        public bool Disponivel { get; set; }
+
+        public Livro(string titulo, string autor, string isbn)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            ISBN = isbn;
+            Disponivel = true;
+        }
+
+        public void Emprestar()
+        {
+            if (Disponivel)
+            {
+                Disponivel = false;
+                Console.WriteLine($"{Titulo} foi emprestado.");
+            }
+            else
+            {
+                Console.WriteLine($"{Titulo} não está disponível.");
+            }
+        }
+
+        public void Devolver()
+        {
+            Disponivel = true;
+            Console.WriteLine($"{Titulo} foi devolvido.");
+        }
+    }
+}
